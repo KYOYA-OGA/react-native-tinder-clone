e8cb0641c42988a7c4c1d1ae451e621e34c56282
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2022-02-14 15:05:15 +0700

    created custom tinder component

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import {Text, ImageBackground, View, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import Card from './src/components/TinderCard';
+import users from './assets/data/users.js';
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-        <ImageBackground
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
-          }}
-          style={styles.image}>
-          <View style={styles.cardInner}>
-            <Text style={styles.name}>Vadim</Text>
-            <Text style={styles.bio}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Repudiandae laborum assumenda dolore, quae quia repellendus
-              incidunt maxime ipsa porro rem.
-            </Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <Card user={users[0]} />
     </View>
   );
 };
@@ -29,40 +16,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-  },
-  card: {
-    width: '95%',
-    height: '70%',
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-    elevation: 11,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-    justifyContent: 'flex-end',
-  },
-  cardInner: {
-    padding: 10,
-  },
-  name: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'white',
-    marginBottom: 10,
-  },
-  bio: {
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,
   },
 });
 
